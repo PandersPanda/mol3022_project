8291bfb839552854465dddbaf77e4bb4e9615727
myherik <myherik@gmail.com>
2023-02-23 14:52:19 +0100

    Added flask backen

---
+++ README2.md
@@ -0,0 +1,15 @@
+### HEYO
+
+## Frontend
+
+Frontend ligger i 'mol3022' mappen
+
+gå inn i mol3022 og kjør 'npm start' (må ha nvm/node installert inkl. axios osv.)
+
+## Backend
+
+Det er flask backend og filen er 'app.py'
+
+for API-er gå inn i 'api'
+
+husk å installer flask (muligens også flask_restful og flask_cors etc.)
--- mol3022/src/Input.tsx
+++ mol3022/src/Input.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import axios from 'axios';
 
 function Input() {
+
+  const [getMessage, setGetMessage] = useState('')
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/flask/hello').then(response => {
+      console.log("SUCCESS", response)
+      setGetMessage(response.data.message)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
+
   return (
+    <div>
     <Box
     component="form"
     sx={{
@@ -17,6 +32,15 @@ function Input() {
       color="secondary" 
       focused />
   </Box>
+
+    {/* div that retrieves the message from getMessage */} 
+    <div>{getMessage}</div>
+
+
+  
+  
+
+  </div>
   )
 }
 
