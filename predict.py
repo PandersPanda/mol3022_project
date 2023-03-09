282e6e1905deb2dfff9e56ee7836fc4f5bedfeae
myherik <myherik@gmail.com>
2023-03-09 13:52:55 +0100

    en god start

--- predict.py
+++ predict.py
@@ -3,20 +3,12 @@ from keras import models
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
 from modelv2 import tokenizer_seq, tokenizer_struc, seq2ngrams, max_length
+from keras.preprocessing.text import Tokenizer
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 model = models.load_model('protein_model2.h5', compile=False)
-
-#model.summary()
-
-#Encode the sequences with padding
-
-sequences = tokenizer_seq.texts_to_sequences(sequences)
-input_data = pad_sequences(sequences, maxlen=128, padding='post')
-
-#y_pred = model.predict(sequences)
 
 def onehot_to_seq(oh_seq, index):
     s = ''
@@ -27,34 +19,27 @@ def onehot_to_seq(oh_seq, index):
         else:
             break
     return s
-
-def plot_results(x, y_):
-    print("---")
-    print("Input: " + str(x))
-    #print("Target: " + str(onehot_to_seq(y, revsere_decoder_index).upper()))
-    print("Result: " + str(onehot_to_seq(y_, revsere_decoder_index).upper()))
-    #fig = plt.figure(figsize=(10,2))
-    #plt.imshow(y.T, cmap='Blues')
-    #plt.imshow(y_.T, cmap='Reds', alpha=.5)
-    #plt.yticks(range(4), [' '] + [revsere_decoder_index[i+1].upper() for i in range(3)])
-    #plt.show()
     
 revsere_decoder_index = {value:key for key,value in tokenizer_struc.word_index.items()}
 revsere_encoder_index = {value:key for key,value in tokenizer_seq.word_index.items()}
 
-sequence = ['HEI']
-sequences = seq2ngrams(sequence)
-sequences = tokenizer_seq.texts_to_sequences(sequences)
-sequences = pad_sequences(sequences, maxlen=max_length, padding='post')
-y_pred = model.predict(sequences)
-plot_results(sequence, y_pred[0])
+sequence = ['SRGTQTE']
+#seq = seq2ngrams(sequence)
+#print(seq)
 
-# N=3
-# y_train_pred = model.predict(X_train[:N])
-# y_test_pred = model.predict(X_test[:N])
-# print('training')
-# for i in range(N):
-#     plot_results('HEI', y_train[i], y_train_pred[i])
-# print('testing')
-# for i in range(N):
-#     plot_results('HEI', y_test[i], y_test_pred[i])
+seq = [sequence[i:i + 3] for i in range(len(sequence))]
+
+#seq = np.array([seq, []], object)[0]
+
+tokenizer_seq = Tokenizer()
+tokenizer_seq.fit_on_texts(seq)
+
+seq = tokenizer_seq.texts_to_sequences(seq)
+seq = pad_sequences(seq, maxlen=max_length, padding='post')
+y_pred = model.predict(seq)
+
+print("y_pred:" + str(y_pred))
+
+print("---")
+print("Input: " + str(sequence[0]))
+print("Result: " + str(onehot_to_seq(y_pred[0], revsere_decoder_index).upper()))
